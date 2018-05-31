2c4180f5df0d93faee4032dc7a50dff467c5b975
p4ranoyd <roberto_adelino_98@hotmail.com>
2018-05-31 14:37:48 +0100

    Created Filmes table and Class

---
+++ app/src/main/java/com/example/acer/cinema/DbTableFilmes.java
@@ -0,0 +1,48 @@
+package com.example.acer.cinema;
+
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.provider.BaseColumns;
+
+public class DbTableFilmes implements BaseColumns {
+
+    public static final String TABLE_NAME = "filmes";
+    private static final String FIELD_NAME = "nome";
+    private static final String FIELD_DATE = "data";
+    private static final String FIELD_CLASSIFICATION = "classificacao";
+
+    private SQLiteDatabase db;
+
+    public DbTableFilmes(SQLiteDatabase db) {
+        this.db = db;
+    }
+
+    public void create() {
+        db.execSQL(
+                "CREATE TABLE " + TABLE_NAME + " (" +
+                        _ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                        FIELD_NAME + " TEXT NOT NULL," + FIELD_DATE+ "DATE NOT NULL,"+ FIELD_CLASSIFICATION+"INTEGER NOT NULL"+
+                        ")"
+        );
+    }
+
+    public static ContentValues getContentValues(Filmes filmes) {
+        ContentValues values = new ContentValues();
+
+        values.put(_ID, filmes.getId());
+        values.put(FIELD_NAME, filmes.getName());
+        /*
+        *
+        DATA
+        *
+        */
+
+        values.put(FIELD_CLASSIFICATION, filmes.getClassificacao());
+
+        return values;
+    }
+
+    public long insert(ContentValues values) {
+        return db.insert(TABLE_NAME, null, values);
+    }
+}
---
+++ app/src/main/java/com/example/acer/cinema/Filmes.java
@@ -0,0 +1,41 @@
+package com.example.acer.cinema;
+
+public class Filmes {
+    private int id;
+    private int classificacao;
+    private String name;
+    /*
+    *
+    *
+    *
+    DATA
+    *
+    *
+    *
+    *
+    */
+    public int getClassificacao() {
+        return classificacao;
+    }
+
+    public void setClassificacao(int classificacao) {
+        this.classificacao = classificacao;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
