a7a9a3d0ce22d414b4638810c20c75a8b4a10a19
p4ranoyd <roberto_adelino_98@hotmail.com>
2018-05-31 14:14:28 +0100

    Added Cinema database Open Helper Class

---
+++ app/src/main/java/com/example/acer/cinema/DbCinemaOpenHelper.java
@@ -0,0 +1,28 @@
+package com.example.acer.cinema;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DbCinemaOpenHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "cinema.db";
+    private static final int DATABASE_VERSION = 1;
+
+
+    public DbCinemaOpenHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
