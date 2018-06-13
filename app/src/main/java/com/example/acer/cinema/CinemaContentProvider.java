4204ac6066168a047c9b53a622f51acc4ee4cdd3
p4ranoyd <roberto_adelino_98@hotmail.com>
2018-06-13 17:17:15 +0100

    Implemented query (Cinema content provider)

--- app/src/main/java/com/example/acer/cinema/CinemaContentProvider.java
+++ app/src/main/java/com/example/acer/cinema/CinemaContentProvider.java
@@ -2,12 +2,34 @@ package com.example.acer.cinema;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 public class CinemaContentProvider extends ContentProvider {
+
+    private static final int FILMES = 100;
+    private static final int FILMES_ID = 101;
+    private static final int CLASSIFICACAO = 200;
+    private static final int CLASSIFICACAO_ID = 201;
+
+
+    private static UriMatcher getCinemaUriMatcher() {
+                UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+                        uriMatcher.addURI("com.example.acer.cinema", "filmes", FILMES);
+                uriMatcher.addURI("com.example.acer.cinema", "filmes/#", FILMES_ID);
+
+                        uriMatcher.addURI("com.example.acer.cinema", "classificacao", CLASSIFICACAO);
+                uriMatcher.addURI("com.example.acer.cinema", "classificacao/#", CLASSIFICACAO_ID);
+
+                        return uriMatcher;
+            }
+
+
 
     DbCinemaOpenHelper cinemaOpenHelper;
 
@@ -20,7 +42,32 @@ public class CinemaContentProvider extends ContentProvider {
     @Nullable
     @Override
     public Cursor query(@NonNull Uri uri, @Nullable String[] strings, @Nullable String s, @Nullable String[] strings1, @Nullable String s1) {
-        return null;
+
+        SQLiteDatabase db = cinemaOpenHelper.getReadableDatabase();
+
+                        String id = uri.getLastPathSegment();
+
+                        UriMatcher matcher = getCinemaUriMatcher();
+
+                        switch (matcher.match(uri)) {
+                       case FILMES:
+                                return new DbTableFilmes(db).query(projection, selection, selectionArgs, null, null, sortOrder);
+
+                        case CLASSIFICACAO:
+                                return new DbTableClassificacao(db).query(projection, selection, selectionArgs, null, null, sortOrder);
+
+                        case FILMES_ID:
+                                return new DbTableFilmes(db).query(projection, DbTableFilmes._ID + "=?", new String[] { id }, null, null, null);
+
+                        case CLASSIFICACAO_ID:
+                                return new DbTableClassificacao(db).query(projection, DbTableClassificacao._ID + "=?", new String[] { id }, null, null, null);
+
+                        default:
+                                throw new UnsupportedOperationException("Invalid URI: " + uri);
+                        }
+
+
+
     }
 
     @Nullable
