06f60eedb209dc50c8aaf27cc4e814f10042a147
p4ranoyd <roberto_adelino_98@hotmail.com>
2018-06-26 17:18:58 +0100

    Changed CinemaContentProvider

--- app/src/main/java/com/example/acer/cinema/CinemaContentProvider.java
+++ app/src/main/java/com/example/acer/cinema/CinemaContentProvider.java
@@ -3,82 +3,184 @@ package com.example.acer.cinema;
 import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.content.UriMatcher;
+import android.database.ContentObserver;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.net.Uri;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 public class CinemaContentProvider extends ContentProvider {
 
-    private static final String AUTHORITY = "com.example.cinema";
-
+    private static final String AUTHORITY = "com.example.acer.cinema";
 
     public static final Uri BASE_URI = Uri.parse("content://" + AUTHORITY);
-    public static final Uri CINEMA_URI = Uri.withAppendedPath(BASE_URI, DbTableFilmes.TABLE_NAME);
-
+    public static final Uri FILMES_URI = Uri.withAppendedPath(BASE_URI, DbTableFilmes.TABLE_NAME);
+    public static final Uri CATEGORIES_URI = Uri.withAppendedPath(BASE_URI, DbTableCategories.TABLE_NAME);
 
     private static final int FILMES = 100;
     private static final int FILMES_ID = 101;
-    private static final int CLASSIFICACAO = 200;
-    private static final int CLASSIFICACAO_ID = 201;
+    private static final int CATEGORIES = 200;
+    private static final int CATEGORIES_ID = 201;
+
     private static final String MULTIPLE_ITEMS = "vnd.android.cursor.dir";
     private static final String SINGLE_ITEM = "vnd.android.cursor.item";
 
-
-    private static UriMatcher getCinemaUriMatcher() {
-                UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
-
-                        uriMatcher.addURI("AUTHORITY", "filmes", FILMES);
-                uriMatcher.addURI("AUTHORITY", "filmes/#", FILMES_ID);
-
-                        uriMatcher.addURI("AUTHORITY", "classificacao", CLASSIFICACAO);
-                uriMatcher.addURI("AUTHORITY", "classificacao/#", CLASSIFICACAO_ID);
-
-                        return uriMatcher;
-            }
-
-
-
     DbCinemaOpenHelper cinemaOpenHelper;
 
+    private static UriMatcher getCinemaUriMatcher() {
+        UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+        uriMatcher.addURI(AUTHORITY, "filmes", FILMES);
+        uriMatcher.addURI(AUTHORITY, "filmes/#", FILMES_ID);
+
+        uriMatcher.addURI(AUTHORITY, "categories", CATEGORIES);
+        uriMatcher.addURI(AUTHORITY, "categories/#", CATEGORIES_ID);
+
+        return uriMatcher;
+    }
+
+
+    /**
+     * Implement this to initialize your content provider on startup.
+     * This method is called for all registered content providers on the
+     * application main thread at application launch time.  It must not perform
+     * lengthy operations, or application startup will be delayed.
+     * <p>
+     * <p>You should defer nontrivial initialization (such as opening,
+     * upgrading, and scanning databases) until the content provider is used
+     * (via {@link #query}, {@link #insert}, etc).  Deferred initialization
+     * keeps application startup fast, avoids unnecessary work if the provider
+     * turns out not to be needed, and stops database errors (such as a full
+     * disk) from halting application launch.
+     * <p>
+     * <p>If you use SQLite, {@link SQLiteOpenHelper}
+     * is a helpful utility class that makes it easy to manage databases,
+     * and will automatically defer opening until first use.  If you do use
+     * SQLiteOpenHelper, make sure to avoid calling
+     * {@link SQLiteOpenHelper#getReadableDatabase} or
+     * {@link SQLiteOpenHelper#getWritableDatabase}
+     * from this method.  (Instead, override
+     * {@link SQLiteOpenHelper#onOpen} to initialize the
+     * database when it is first opened.)
+     *
+     * @return true if the provider was successfully loaded, false otherwise
+     */
     @Override
     public boolean onCreate() {
         cinemaOpenHelper = new DbCinemaOpenHelper(getContext());
-        return false;
+
+        return true;
     }
 
+    /**
+     * Implement this to handle query requests from clients.
+     * <p>
+     * <p>Apps targeting {@link Build.VERSION_CODES#O} or higher should override
+     * {@link #query(Uri, String[], Bundle, CancellationSignal)} and provide a stub
+     * implementation of this method.
+     * <p>
+     * <p>This method can be called from multiple threads, as described in
+     * <a href="{@docRoot}guide/topics/fundamentals/processes-and-threads.html#Threads">Processes
+     * and Threads</a>.
+     * <p>
+     * Example client call:<p>
+     * <pre>// Request a specific record.
+     * Cursor managedCursor = managedQuery(
+     * ContentUris.withAppendedId(Contacts.People.CONTENT_URI, 2),
+     * projection,    // Which columns to return.
+     * null,          // WHERE clause.
+     * null,          // WHERE clause value substitution
+     * People.NAME + " ASC");   // Sort order.</pre>
+     * Example implementation:<p>
+     * <pre>// SQLiteQueryBuilder is a helper class that creates the
+     * // proper SQL syntax for us.
+     * SQLiteQueryBuilder qBuilder = new SQLiteQueryBuilder();
+     *
+     * // Set the table we're querying.
+     * qBuilder.setTables(DATABASE_TABLE_NAME);
+     *
+     * // If the query ends in a specific record number, we're
+     * // being asked for a specific record, so set the
+     * // WHERE clause in our query.
+     * if((URI_MATCHER.match(uri)) == SPECIFIC_MESSAGE){
+     * qBuilder.appendWhere("_id=" + uri.getPathLeafId());
+     * }
+     *
+     * // Make the query.
+     * Cursor c = qBuilder.query(mDb,
+     * projection,
+     * selection,
+     * selectionArgs,
+     * groupBy,
+     * having,
+     * sortOrder);
+     * c.setNotificationUri(getContext().getContentResolver(), uri);
+     * return c;</pre>
+     *
+     * @param uri           The URI to query. This will be the full URI sent by the client;
+     *                      if the client is requesting a specific record, the URI will end in a record number
+     *                      that the implementation should parse and add to a WHERE or HAVING clause, specifying
+     *                      that _id value.
+     * @param projection    The list of columns to put into the cursor. If
+     *                      {@code null} all columns are included.
+     * @param selection     A selection criteria to apply when filtering rows.
+     *                      If {@code null} then all rows are included.
+     * @param selectionArgs You may include ?s in selection, which will be replaced by
+     *                      the values from selectionArgs, in order that they appear in the selection.
+     *                      The values will be bound as Strings.
+     * @param sortOrder     How the rows in the cursor should be sorted.
+     *                      If {@code null} then the provider is free to define the sort order.
+     * @return a Cursor or {@code null}.
+     */
     @Nullable
     @Override
-        public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder){
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
         SQLiteDatabase db = cinemaOpenHelper.getReadableDatabase();
 
-                        String id = uri.getLastPathSegment();
+        String id = uri.getLastPathSegment();
 
-                        UriMatcher matcher = getCinemaUriMatcher();
+        UriMatcher matcher = getCinemaUriMatcher();
 
-                        switch (matcher.match(uri)) {
-                       case FILMES:
-                                return new DbTableFilmes(db).query(projection, selection, selectionArgs, null, null, sortOrder);
+        switch (matcher.match(uri)) {
+            case FILMES:
+                return new DbTableFilmes(db).query(projection, selection, selectionArgs, null, null, sortOrder);
 
-                        case CLASSIFICACAO:
-                                return new DbTableCategories(db).query(projection, selection, selectionArgs, null, null, sortOrder);
+            case CATEGORIES:
+                return new DbTableCategories(db).query(projection, selection, selectionArgs, null, null, sortOrder);
 
-                        case FILMES_ID:
-                                return new DbTableFilmes(db).query(projection, DbTableFilmes._ID + "=?", new String[] { id }, null, null, null);
+            case FILMES_ID:
+                return new DbTableFilmes(db).query(projection, DbTableFilmes._ID + "=?", new String[] { id }, null, null, null);
 
-                        case CLASSIFICACAO_ID:
-                                return new DbTableCategories(db).query(projection, DbTableCategories._ID + "=?", new String[] { id }, null, null, null);
+            case CATEGORIES_ID:
+                return new DbTableCategories(db).query(projection, DbTableCategories._ID + "=?", new String[] { id }, null, null, null);
 
-                        default:
-                                throw new UnsupportedOperationException("Invalid URI: " + uri);
-                        }
-
-
-
+            default:
+                throw new UnsupportedOperationException("Invalid URI: " + uri);
+        }
     }
 
+    /**
+     * Implement this to handle requests for the MIME type of the data at the
+     * given URI.  The returned MIME type should start with
+     * <code>vnd.android.cursor.item</code> for a single record,
+     * or <code>vnd.android.cursor.dir/</code> for multiple items.
+     * This method can be called from multiple threads, as described in
+     * <a href="{@docRoot}guide/topics/fundamentals/processes-and-threads.html#Threads">Processes
+     * and Threads</a>.
+     * <p>
+     * <p>Note that there are no permissions needed for an application to
+     * access this information; if your content provider requires read and/or
+     * write permissions, or is not exported, all applications can still call
+     * this method regardless of their access permissions.  This allows them
+     * to retrieve the MIME type for a URI when dispatching intents.
+     *
+     * @param uri the URI to query.
+     * @return a MIME type string, or {@code null} if there is no type.
+     */
     @Nullable
     @Override
     public String getType(@NonNull Uri uri) {
@@ -88,88 +190,90 @@ public class CinemaContentProvider extends ContentProvider {
             case FILMES:
                 return MULTIPLE_ITEMS + "/" + AUTHORITY + "/" + DbTableFilmes.TABLE_NAME;
 
-            case CLASSIFICACAO:
+            case CATEGORIES:
                 return MULTIPLE_ITEMS + "/" + AUTHORITY + "/" + DbTableCategories.TABLE_NAME;
 
             case FILMES_ID:
                 return SINGLE_ITEM + "/" + AUTHORITY + "/" + DbTableFilmes.TABLE_NAME;
 
-            case CLASSIFICACAO_ID:
-                return SINGLE_ITEM + "/" + AUTHORITY + "/" + DbTableCategories.TABLE_NAME;
+            case CATEGORIES_ID:
+                return SINGLE_ITEM  + "/" + AUTHORITY + "/" + DbTableCategories.TABLE_NAME;
 
             default:
                 throw new UnsupportedOperationException("Unknown URI: " + uri);
-
         }
     }
-    @Nullable
 
+    /**
+     * Implement this to handle requests to insert a new row.
+     * As a courtesy, call {@link ContentResolver#notifyChange(Uri, ContentObserver) notifyChange()}
+     * after inserting.
+     * This method can be called from multiple threads, as described in
+     * <a href="{@docRoot}guide/topics/fundamentals/processes-and-threads.html#Threads">Processes
+     * and Threads</a>.
+     *
+     * @param uri    The content:// URI of the insertion request. This must not be {@code null}.
+     * @param values A set of column_name/value pairs to add to the database.
+     *               This must not be {@code null}.
+     * @return The URI for the newly inserted item.
+     */
+    @Nullable
     @Override
     public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
         SQLiteDatabase db = cinemaOpenHelper.getWritableDatabase();
 
         UriMatcher matcher = getCinemaUriMatcher();
+
         long id = -1;
 
-
         switch (matcher.match(uri)) {
-                        case FILMES:
-                                id = new DbTableFilmes(db).insert(values);
-                                break;
-                        case CLASSIFICACAO:
-                                id = new DbTableCategories(db).insert(values);
-                                break;
+            case FILMES:
+                id = new DbTableFilmes(db).insert(values);
+                break;
 
-                        default:
-                                throw new UnsupportedOperationException("Invalid URI: " + uri);
-                        }
+            case CATEGORIES:
+                id = new DbTableCategories(db).insert(values);
+                break;
 
-                if (id > 0) {
-                        notifyChanges(uri);
-                        return Uri.withAppendedPath(uri, Long.toString(id));
-                    } else {
-                        throw new SQLException("Could not insert record");
-                    }
-           }
+            default:
+                throw new UnsupportedOperationException("Invalid URI: " + uri);
+        }
 
-            private void notifyChanges(@NonNull Uri uri) {
-                getContext().getContentResolver().notifyChange(uri, null);
-
-
-
+        if (id > 0) {
+            notifyChanges(uri);
+            return Uri.withAppendedPath(uri, Long.toString(id));
+        } else {
+            throw new SQLException("Could not insert record");
+        }
     }
 
+    private void notifyChanges(@NonNull Uri uri) {
+        getContext().getContentResolver().notifyChange(uri, null);
+    }
+
+    /**
+     * Implement this to handle requests to delete one or more rows.
+     * The implementation should apply the selection clause when performing
+     * deletion, allowing the operation to affect multiple rows in a directory.
+     * As a courtesy, call {@link ContentResolver#notifyChange(Uri, ContentObserver) notifyChange()}
+     * after deleting.
+     * This method can be called from multiple threads, as described in
+     * <a href="{@docRoot}guide/topics/fundamentals/processes-and-threads.html#Threads">Processes
+     * and Threads</a>.
+     * <p>
+     * <p>The implementation is responsible for parsing out a row ID at the end
+     * of the URI, if a specific row is being deleted. That is, the client would
+     * pass in <code>content://contacts/people/22</code> and the implementation is
+     * responsible for parsing the record number (22) when creating a SQL statement.
+     *
+     * @param uri           The full URI to query, including a row ID (if a specific record is requested).
+     * @param selection     An optional restriction to apply to rows when deleting.
+     * @param selectionArgs
+     * @return The number of rows affected.
+     * @throws SQLException
+     */
     @Override
     public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
-                SQLiteDatabase db = cinemaOpenHelper.getWritableDatabase();
-
-                        UriMatcher matcher = getCinemaUriMatcher();
-
-                        String id = uri.getLastPathSegment();
-
-                        int rows = 0;
-
-                        switch (matcher.match(uri)) {
-                        case FILMES_ID:
-                                rows = new DbTableFilmes(db).delete(DbTableFilmes._ID +"=?", new String [] { id });
-                                break;
-
-                        case CLASSIFICACAO_ID:
-                                rows = new DbTableCategories(db).delete(DbTableCategories._ID +"=?", new String [] { id });
-                                break;
-
-                        default:
-                                throw new UnsupportedOperationException("Invalid URI: " + uri);
-                        }
-
-                        if (rows > 0) notifyChanges(uri);
-
-                        return rows;
-    }
-
-    @Override
-    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
-
         SQLiteDatabase db = cinemaOpenHelper.getWritableDatabase();
 
         UriMatcher matcher = getCinemaUriMatcher();
@@ -180,10 +284,11 @@ public class CinemaContentProvider extends ContentProvider {
 
         switch (matcher.match(uri)) {
             case FILMES_ID:
-                rows = new DbTableFilmes(db).update(values, DbTableFilmes._ID + "=?", new String[]{id});
+                rows = new DbTableFilmes(db).delete(DbTableFilmes._ID +"=?", new String [] { id });
                 break;
-            case CLASSIFICACAO_ID:
-                rows = new DbTableCategories(db).update(values, DbTableCategories._ID + "=?", new String[]{id});
+
+            case CATEGORIES_ID:
+                rows = new DbTableCategories(db).delete(DbTableCategories._ID +"=?", new String [] { id });
                 break;
 
             default:
@@ -193,6 +298,51 @@ public class CinemaContentProvider extends ContentProvider {
         if (rows > 0) notifyChanges(uri);
 
         return rows;
+    }
 
+    /**
+     * Implement this to handle requests to update one or more rows.
+     * The implementation should update all rows matching the selection
+     * to set the columns according to the provided values map.
+     * As a courtesy, call {@link ContentResolver#notifyChange(Uri, ContentObserver) notifyChange()}
+     * after updating.
+     * This method can be called from multiple threads, as described in
+     * <a href="{@docRoot}guide/topics/fundamentals/processes-and-threads.html#Threads">Processes
+     * and Threads</a>.
+     *
+     * @param uri           The URI to query. This can potentially have a record ID if this
+     *                      is an update request for a specific record.
+     * @param values        A set of column_name/value pairs to update in the database.
+     *                      This must not be {@code null}.
+     * @param selection     An optional filter to match rows to update.
+     * @param selectionArgs
+     * @return the number of rows affected.
+     */
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        SQLiteDatabase db = cinemaOpenHelper.getWritableDatabase();
+
+        UriMatcher matcher = getCinemaUriMatcher();
+
+        String id = uri.getLastPathSegment();
+
+        int rows = 0;
+
+        switch (matcher.match(uri)) {
+            case FILMES_ID:
+                rows = new DbTableFilmes(db).update(values, DbTableFilmes._ID +"=?", new String [] { id });
+                break;
+
+            case CATEGORIES_ID:
+                rows = new DbTableCategories(db).update(values, DbTableCategories._ID +"=?", new String [] { id });
+                break;
+
+            default:
+                throw new UnsupportedOperationException("Invalid URI: " + uri);
+        }
+
+        if (rows > 0) notifyChanges(uri);
+
+        return rows;
     }
 }
